3c9aaafa03b7d21d7c2066ee5accfab409e38bfe
Martijn Kunstman <martijn@kunstman.nl>
2024-12-01 10:28:18 +0100

    day 1

--- day1/main.js
+++ day1/main.js
@@ -1,29 +1,31 @@
-let data;
-let answer1 = 0;
-let answer2 = 0;
 fetch("data.txt")
   .then((response) => response.text())
   .then((textData) => {
-    console.log(textData);
-
-    //in this example we deserialize and convert the textData into an array of numbers
-    data = textData
-      .replace(/(\r\n|\n|\r)/gm, "\n")
-      .split("\n")
-      .map((value) => Number(value));
-    console.log(data);
-  });
-
-
-function part1() {
-  // your code here
-  document.getElementById("solution_part_1").innerHTML = answer1;
-} 
-
-function part2() {
-  // your code here
-  document.getElementById("solution_part_2").innerHTML = answer2;
-}
-
-part1();
-part2();
+    let array1 = [];
+    let array2 = [];
+    let answer1 = 0;
+    let answer2 = 0;
+    textData.replace(/(\r\n|\n|\r)/gm, "\n").split("\n").map((x) => {
+      array1.push(Number(x.split("   ")[0]));
+      array2.push(Number(x.split("   ")[1]));
+    });
+    //part 1
+    array1.sort();
+    array2.sort();
+    for (i = 0; i < array1.length; i++) {
+      answer1 += Math.abs(array1[i] - array2[i]);
+    }
+    console.log(answer1);
+    //part 2
+    for (a = 0; a < array1.length; a++) {
+      let count = 0;
+      for (b = 0; b < array2.length; b++) {
+        if (array2[b] === array1[a]) {
+          count++;
+          //optimise by removing the element from the array...
+        }
+      }
+      answer2 += count * array1[a];
+    }
+    console.log(answer2);
+  });
