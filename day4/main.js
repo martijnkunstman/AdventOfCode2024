7fe5f0506ca57f7b587354a2c11a1e7e47cdecb8
Martijn Kunstman <martijn@kunstman.nl>
2024-12-04 23:55:10 +0100

    day 4 optimize useUintArray

--- day4/main.js
+++ day4/main.js
@@ -17,7 +17,7 @@
 let answer1 = 0;
 let answer2 = 0;
 let data;
-let dataUint8;
+let dataUint;
 let length;
 //fetch("data_simple.txt")
 fetch("data.txt")
@@ -32,38 +32,42 @@ fetch("data.txt")
 
     /*
     //twp dimensional array
-    dataUint8 = new Array(length);
+    dataUint = new Array(length);
     for (let i = 0; i < length; i++) {
-      dataUint8[i] = new Uint8Array(length);
+      dataUint[i] = new Uint8Array(length);
       for (let j = 0; j < length; j++) {
-        if (data[i][j] == "X") dataUint8[i][j] = 1;
-        if (data[i][j] == "M") dataUint8[i][j] = 2;
-        if (data[i][j] == "A") dataUint8[i][j] = 4;
-        if (data[i][j] == "S") dataUint8[i][j] = 8;
+        if (data[i][j] == "X") dataUint[i][j] = 1;
+        if (data[i][j] == "M") dataUint[i][j] = 2;
+        if (data[i][j] == "A") dataUint[i][j] = 4;
+        if (data[i][j] == "S") dataUint[i][j] = 8;
       }
     }
     */
 
     //one dimensional array
-    dataUint8 = new Uint8Array(length * length);
+    dataUint = new Uint16Array(length * length);
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length; j++) {
-        const idx = i * length + j;
-        if (data[i][j] == "X") dataUint8[idx] = 1;
-        if (data[i][j] == "M") dataUint8[idx] = 2;
-        if (data[i][j] == "A") dataUint8[idx] = 4;
-        if (data[i][j] == "S") dataUint8[idx] = 8;
+        const idx = j * length + i;
+        if (i > length - 1 || j > length - 1) {
+          dataUint[idx] = 0;
+        } else {
+          if (data[i][j] == "X") dataUint[idx] = 1;
+          if (data[i][j] == "M") dataUint[idx] = 10;
+          if (data[i][j] == "A") dataUint[idx] = 100;
+          if (data[i][j] == "S") dataUint[idx] = 1000;
+        }
       }
     }
-
     //-------------------
     let currentTime = performance.timeOrigin + performance.now();
     useUint8Array();
     console.log(
       "duration:" + (performance.timeOrigin + performance.now() - currentTime)
     );
-    console.log("answer1: " + answer1); //2718
-    console.log("answer2: " + answer2); //2046
+    console.log("-useUintArray array-");
+    console.log("answer1: " + answer1 + " should be: 2718"); //2718
+    console.log("answer2: " + answer2 + " should be: 2046"); //2046
 
     //check time when convert to numbers and use Uint8Array
     //-------------------
@@ -71,11 +75,12 @@ fetch("data.txt")
     answer2 = 0;
     currentTime = performance.timeOrigin + performance.now();
     useNormalArray();
+    console.log("-normal array-");
     console.log(
       "duration:" + (performance.timeOrigin + performance.now() - currentTime)
     );
-    console.log("answer1: " + answer1); //2718
-    console.log("answer2: " + answer2); //2046
+    console.log("answer1: " + answer1 + " should be: 2718"); //2718
+    console.log("answer2: " + answer2 + " should be: 2046"); //2046
   });
 
 //---------------------------------
@@ -163,6 +168,10 @@ function useNormalArray() {
 }
 
 //---------------------------------
+
+//correct answer is 1*1 + 10*2 + 100*3 + 1000*4 = 4321
+//correct answer is 1000*1 + 100*2 + 20*3 + 1*4 = 1234
+
 function useUint8Array() {
   for (let y = 0; y < length; y++) {
     for (let x = 0; x < length; x++) {
@@ -171,18 +180,20 @@ function useUint8Array() {
       // Horizontal -
       if (x < length - 3) {
         if (
-          dataUint8[idx] == 1 &&
-          dataUint8[idx + 1] == 2 &&
-          dataUint8[idx + 2] == 4 &&
-          dataUint8[idx + 3] == 8
+          dataUint[idx] +
+            dataUint[idx + 1] * 2 +
+            dataUint[idx + 2] * 3 +
+            dataUint[idx + 3] * 4 ==
+          4321
         ) {
           answer1++;
         }
         if (
-          dataUint8[idx] == 8 &&
-          dataUint8[idx + 1] == 4 &&
-          dataUint8[idx + 2] == 2 &&
-          dataUint8[idx + 3] == 1
+          dataUint[idx] * 4 +
+            dataUint[idx + 1] * 3 +
+            dataUint[idx + 2] * 2 +
+            dataUint[idx + 3] ==
+          1234
         ) {
           answer1++;
         }
@@ -191,18 +202,20 @@ function useUint8Array() {
       // Vertical |
       if (y < length - 3) {
         if (
-          dataUint8[idx] == 1 &&
-          dataUint8[idx + length] == 2 &&
-          dataUint8[idx + 2 * length] == 4 &&
-          dataUint8[idx + 3 * length] == 8
+          dataUint[idx] +
+            dataUint[idx + length] * 2 +
+            dataUint[idx + 2 * length] * 3 +
+            dataUint[idx + 3 * length] * 4 ==
+          4321
         ) {
           answer1++;
         }
         if (
-          dataUint8[idx] == 8 &&
-          dataUint8[idx + length] == 4 &&
-          dataUint8[idx + 2 * length] == 2 &&
-          dataUint8[idx + 3 * length] == 1
+          dataUint[idx] * 4 +
+            dataUint[idx + length] * 3 +
+            dataUint[idx + 2 * length] * 2 +
+            dataUint[idx + 3 * length] ==
+          1234
         ) {
           answer1++;
         }
@@ -211,18 +224,20 @@ function useUint8Array() {
       // Diagonal \
       if (x < length - 3 && y < length - 3) {
         if (
-          dataUint8[idx] == 1 &&
-          dataUint8[idx + length + 1] == 2 &&
-          dataUint8[idx + 2 * (length + 1)] == 4 &&
-          dataUint8[idx + 3 * (length + 1)] == 8
+          dataUint[idx] +
+            dataUint[idx + length + 1] * 2 +
+            dataUint[idx + 2 * (length + 1)] * 3 +
+            dataUint[idx + 3 * (length + 1)] * 4 ==
+          4321
         ) {
           answer1++;
         }
         if (
-          dataUint8[idx] == 8 &&
-          dataUint8[idx + length + 1] == 4 &&
-          dataUint8[idx + 2 * (length + 1)] == 2 &&
-          dataUint8[idx + 3 * (length + 1)] == 1
+          dataUint[idx] * 4 +
+            dataUint[idx + length + 1] * 3 +
+            dataUint[idx + 2 * (length + 1)] * 2 +
+            dataUint[idx + 3 * (length + 1)] ==
+          1234
         ) {
           answer1++;
         }
@@ -231,18 +246,20 @@ function useUint8Array() {
       // Diagonal /
       if (x > 2 && y < length - 3) {
         if (
-          dataUint8[idx] == 1 &&
-          dataUint8[idx + length - 1] == 2 &&
-          dataUint8[idx + 2 * (length - 1)] == 4 &&
-          dataUint8[idx + 3 * (length - 1)] == 8
+          dataUint[idx] +
+            dataUint[idx + length - 1] * 2 +
+            dataUint[idx + 2 * (length - 1)] * 3 +
+            dataUint[idx + 3 * (length - 1)] * 4 ==
+          4321
         ) {
           answer1++;
         }
         if (
-          dataUint8[idx] == 8 &&
-          dataUint8[idx + length - 1] == 4 &&
-          dataUint8[idx + 2 * (length - 1)] == 2 &&
-          dataUint8[idx + 3 * (length - 1)] == 1
+          dataUint[idx] * 4 +
+            dataUint[idx + length - 1] * 3 +
+            dataUint[idx + 2 * (length - 1)] * 2 +
+            dataUint[idx + 3 * (length - 1)] ==
+          1234
         ) {
           answer1++;
         }
@@ -251,36 +268,36 @@ function useUint8Array() {
       // 3x3 patterns
       if (x < length - 2 && y < length - 2) {
         const centerIdx = idx + length + 1; // Center index of the 3x3 grid
-        if (dataUint8[centerIdx] == 4) {
+        if (dataUint[centerIdx] == 100) {
           if (
-            dataUint8[idx] == 2 &&
-            dataUint8[idx + 2] == 2 &&
-            dataUint8[idx + 2 * length] == 8 &&
-            dataUint8[idx + 2 * length + 2] == 8
+            dataUint[idx] == 10 &&
+            dataUint[idx + 2] == 10 &&
+            dataUint[idx + 2 * length] == 1000 &&
+            dataUint[idx + 2 * length + 2] == 1000
           ) {
             answer2++;
           }
           if (
-            dataUint8[idx] == 2 &&
-            dataUint8[idx + 2] == 8 &&
-            dataUint8[idx + 2 * length] == 2 &&
-            dataUint8[idx + 2 * length + 2] == 8
+            dataUint[idx] == 10 &&
+            dataUint[idx + 2] == 1000 &&
+            dataUint[idx + 2 * length] == 10 &&
+            dataUint[idx + 2 * length + 2] == 1000
           ) {
             answer2++;
           }
           if (
-            dataUint8[idx] == 8 &&
-            dataUint8[idx + 2] == 2 &&
-            dataUint8[idx + 2 * length] == 8 &&
-            dataUint8[idx + 2 * length + 2] == 2
+            dataUint[idx] == 1000 &&
+            dataUint[idx + 2] == 10 &&
+            dataUint[idx + 2 * length] == 1000 &&
+            dataUint[idx + 2 * length + 2] == 10
           ) {
             answer2++;
           }
           if (
-            dataUint8[idx] == 8 &&
-            dataUint8[idx + 2] == 8 &&
-            dataUint8[idx + 2 * length] == 2 &&
-            dataUint8[idx + 2 * length + 2] == 2
+            dataUint[idx] == 1000 &&
+            dataUint[idx + 2] == 1000 &&
+            dataUint[idx + 2 * length] == 10 &&
+            dataUint[idx + 2 * length + 2] == 10
           ) {
             answer2++;
           }
@@ -299,19 +316,19 @@ function useUint8Array() {
       let hor, ver, dia1, dia2;
       //hor
       if (x < length - 3) {
-        if (dataUint8[x][y] == 1) {
-          if (dataUint8[x + 1][y] == 2) {
-            if (dataUint8[x + 2][y] == 4) {
-              if (dataUint8[x + 3][y] == 8) {
+        if (dataUint[x][y] == 1) {
+          if (dataUint[x + 1][y] == 2) {
+            if (dataUint[x + 2][y] == 4) {
+              if (dataUint[x + 3][y] == 8) {
                 answer1++;
               }
             }
           }
         }
-        if (dataUint8[x][y] == 8) {
-          if (dataUint8[x + 1][y] == 4) {
-            if (dataUint8[x + 2][y] == 2) {
-              if (dataUint8[x + 3][y] == 1) {
+        if (dataUint[x][y] == 8) {
+          if (dataUint[x + 1][y] == 4) {
+            if (dataUint[x + 2][y] == 2) {
+              if (dataUint[x + 3][y] == 1) {
                 answer1++;
               }
             }
@@ -320,19 +337,19 @@ function useUint8Array() {
       }
       //ver
       if (y < length - 3) {
-        if (dataUint8[x][y] == 1) {
-          if (dataUint8[x][y + 1] == 2) {
-            if (dataUint8[x][y + 2] == 4) {
-              if (dataUint8[x][y + 3] == 8) {
+        if (dataUint[x][y] == 1) {
+          if (dataUint[x][y + 1] == 2) {
+            if (dataUint[x][y + 2] == 4) {
+              if (dataUint[x][y + 3] == 8) {
                 answer1++;
               }
             }
           }
         }
-        if (dataUint8[x][y] == 8) {
-          if (dataUint8[x][y + 1] == 4) {
-            if (dataUint8[x][y + 2] == 2) {
-              if (dataUint8[x][y + 3] == 1) {
+        if (dataUint[x][y] == 8) {
+          if (dataUint[x][y + 1] == 4) {
+            if (dataUint[x][y + 2] == 2) {
+              if (dataUint[x][y + 3] == 1) {
                 answer1++;
               }
             }
@@ -341,19 +358,19 @@ function useUint8Array() {
       }
       //dia1
       if (x < length - 3 && y < length - 3) {
-        if (dataUint8[x][y] == 1) {
-          if (dataUint8[x + 1][y + 1] == 2) {
-            if (dataUint8[x + 2][y + 2] == 4) {
-              if (dataUint8[x + 3][y + 3] == 8) {
+        if (dataUint[x][y] == 1) {
+          if (dataUint[x + 1][y + 1] == 2) {
+            if (dataUint[x + 2][y + 2] == 4) {
+              if (dataUint[x + 3][y + 3] == 8) {
                 answer1++;
               }
             }
           }
         }
-        if (dataUint8[x][y] == 8) {
-          if (dataUint8[x + 1][y + 1] == 4) {
-            if (dataUint8[x + 2][y + 2] == 2) {
-              if (dataUint8[x + 3][y + 3] == 1) {
+        if (dataUint[x][y] == 8) {
+          if (dataUint[x + 1][y + 1] == 4) {
+            if (dataUint[x + 2][y + 2] == 2) {
+              if (dataUint[x + 3][y + 3] == 1) {
                 answer1++;
               }
             }
@@ -362,19 +379,19 @@ function useUint8Array() {
       }
       //dia2
       if (x > 2 && y < length - 3) {
-        if (dataUint8[x][y] == 1) {
-          if (dataUint8[x - 1][y + 1] == 2) {
-            if (dataUint8[x - 2][y + 2] == 4) {
-              if (dataUint8[x - 3][y + 3] == 8) {
+        if (dataUint[x][y] == 1) {
+          if (dataUint[x - 1][y + 1] == 2) {
+            if (dataUint[x - 2][y + 2] == 4) {
+              if (dataUint[x - 3][y + 3] == 8) {
                 answer1++;
               }
             }
           }
         }
-        if (dataUint8[x][y] == 8) {
-          if (dataUint8[x - 1][y + 1] == 4) {
-            if (dataUint8[x - 2][y + 2] == 2) {
-              if (dataUint8[x - 3][y + 3] == 1) {
+        if (dataUint[x][y] == 8) {
+          if (dataUint[x - 1][y + 1] == 4) {
+            if (dataUint[x - 2][y + 2] == 2) {
+              if (dataUint[x - 3][y + 3] == 1) {
                 answer1++;
               }
             }
@@ -387,36 +404,36 @@ function useUint8Array() {
         // A       A       A       A
         //S S     M S     S M     M M
 
-        if (dataUint8[x + 1][y + 1] == 4) {
+        if (dataUint[x + 1][y + 1] == 4) {
           if (
-            dataUint8[x][y] == 2 &&
-            dataUint8[x + 2][y] == 2 &&
-            dataUint8[x][y + 2] == 8 &&
-            dataUint8[x + 2][y + 2] == 8
+            dataUint[x][y] == 2 &&
+            dataUint[x + 2][y] == 2 &&
+            dataUint[x][y + 2] == 8 &&
+            dataUint[x + 2][y + 2] == 8
           ) {
             answer2++;
           }
           if (
-            dataUint8[x][y] == 2 &&
-            dataUint8[x + 2][y] == 8 &&
-            dataUint8[x][y + 2] == 2 &&
-            dataUint8[x + 2][y + 2] == 8
+            dataUint[x][y] == 2 &&
+            dataUint[x + 2][y] == 8 &&
+            dataUint[x][y + 2] == 2 &&
+            dataUint[x + 2][y + 2] == 8
           ) {
             answer2++;
           }
           if (
-            dataUint8[x][y] == 8 &&
-            dataUint8[x + 2][y] == 2 &&
-            dataUint8[x][y + 2] == 8 &&
-            dataUint8[x + 2][y + 2] == 2
+            dataUint[x][y] == 8 &&
+            dataUint[x + 2][y] == 2 &&
+            dataUint[x][y + 2] == 8 &&
+            dataUint[x + 2][y + 2] == 2
           ) {
             answer2++;
           }
           if (
-            dataUint8[x][y] == 8 &&
-            dataUint8[x + 2][y] == 8 &&
-            dataUint8[x][y + 2] == 2 &&
-            dataUint8[x + 2][y + 2] == 2
+            dataUint[x][y] == 8 &&
+            dataUint[x + 2][y] == 8 &&
+            dataUint[x][y + 2] == 2 &&
+            dataUint[x + 2][y + 2] == 2
           ) {
             answer2++;
           }
