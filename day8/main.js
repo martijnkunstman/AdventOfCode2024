d3a66df33f7e6017771abe13912797370d597f22
Martijn Kunstman <martijn@kunstman.nl>
2024-12-08 16:55:18 +0100

    day 1 part 1 done

---
+++ day8/main.js
@@ -0,0 +1,73 @@
+let answer1 = 0;
+let answer2 = 0;
+let mapData = [];
+let mapData2 = [];
+
+fetch("data.txt")
+//fetch("data_simple.txt")
+  .then((response) => response.text())
+  .then((textData) => {
+    mapData = textData.split("\r\n");
+    for (let i = 0; i < mapData.length; i++) {
+      mapData[i] = mapData[i].split("");
+    }
+
+    //create a new map with the same size
+    for (let i = 0; i < mapData.length; i++) {
+      mapData2.push([]);
+      for (let j = 0; j < mapData[i].length; j++) {
+        mapData2[i].push(0);
+      }
+    }
+
+    console.log(mapData);
+
+    //got through the complete map and find not dots...
+    for (let y = 0; y < mapData.length; y++) {
+      for (let x = 0; x < mapData[y].length; x++) {
+        if (mapData[y][x] != "." && mapData[y][x] != "#") {
+          let charToFind = mapData[y][x];
+          findAndMark(charToFind, x, y);
+        }
+      }
+    }
+
+    console.log(mapData2);
+
+    //count all the #
+    for (let i = 0; i < mapData2.length; i++) {
+      for (let j = 0; j < mapData2[i].length; j++) {
+        if (mapData2[i][j] == 1) {
+          answer1++;
+        }
+      }
+    }
+
+    function findAndMark(charToFind, x1, y1) {
+      for (let y = 0; y < mapData.length; y++) {
+        for (let x = 0; x < mapData[y].length; x++) {
+          if (mapData[y][x] == charToFind) {
+            //found char now reflect it
+            let x2 = x;
+            let y2 = y;
+            let differenceX = x1 - x2;
+            let differenceY = y1 - y2;
+            let newx = x1 + differenceX;
+            let newy = y1 + differenceY;
+            if (
+              (differenceX != 0 && differenceY != 0) &&
+              newx >= 0 &&
+              newx < mapData[0].length &&
+              newy >= 0 &&
+              newy < mapData.length
+            ) {
+              mapData2[newy][newx] = 1;
+            }
+          }
+        }
+      }
+    }
+
+    console.log("answer1: " + answer1); //361 OK!
+    console.log("answer2: " + answer2); //
+  });
