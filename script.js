8ea632bd5e64031a094f9404c0c5aa4b347fe9c2
Martijn Kunstman <martijn@kunstman.nl>
2024-12-08 18:34:29 +0100

    index

---
+++ script.js
@@ -0,0 +1,26 @@
+let day = 26;
+
+for (let i = 1; i < 26; i++) {
+  if (i <= day) {
+  document.getElementById("buttons").innerHTML +=
+    "<button onclick='showDay(" + i + ")'>day " + i + "</button>";
+  }
+  else {
+    document.getElementById("buttons").innerHTML +=
+    "<button disabled style='opacity:0.50;'>day " + i + "</button>";
+  }
+}
+showDay(day);
+function showDay(day) {
+  document.getElementById("answer").innerHTML = "";
+  document.getElementById("code").innerHTML = '<pre><code id="jscode" class="language-javascript"></code></pre>';
+  fetch("day" + day + "/main.js")
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("jscode").innerHTML = data;
+      hljs.highlightAll();
+      //eval(data);
+      
+    });
+}
