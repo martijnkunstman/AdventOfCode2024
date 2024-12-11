b9c7733d2c4353319c0568c7621fa705ea5a4e2d
Martijn Kunstman <martijn@kunstman.nl>
2024-12-11 23:43:23 +0100

    day 11 part 2 tryout

--- day11/main.js
+++ day11/main.js
@@ -1,6 +1,8 @@
 let answer1 = 0;
 let answer2 = 0;
 let stones = [];
+let results = [];
+let numbersAndResultsFor4 = [];
 
 fetch("data.txt")
 //fetch("data_simple.txt")
@@ -11,47 +13,56 @@ fetch("data.txt")
     console.log("stones:");
     console.log(stones);
 
-    //
-    //rules: 0 becomes 1
-    //evendigits: becomes two stones (spit in half, 00 -> 0)
-    //not 0 and not even digits times 2024
-    //
-
-    //25 times
-
-    for (let i = 0; i < 25; i++) {
-      for (let j = 0; j < stones.length; j++) {
-        if (stones[j] == "0") {
-          //rule 1
-          stones[j] = "1";
-        } else if (stones[j].toString().length % 2 == 0) {
-          //rule 2
-          const partOne = stones[j]
-            .toString()
-            .slice(0, stones[j].toString().length / 2);
-          let partTwo = stones[j]
-            .toString()
-            .slice(
-              stones[j].toString().length / 2,
-              stones[j].toString().length
-            );
-          while (partTwo.split("")[0] == 0) {
-            partTwo = partTwo.split("").slice(1, partTwo.length).join("");
-          }
-          if (partTwo == "") {
-            partTwo = "0";
-          }
-          stones[j]=[partOne, partTwo];
-        } else {
-          let bigNumber = BigInt(stones[j]);
-          bigNumber = bigNumber * 2024n;
-          stones[j] = bigNumber.toString();
-        }
-      }
-      stones = stones.flat();
-      console.log("stones:");
-      console.log(stones);
+    for (let i = 0; i < 10; i++) {
+      numbersAndResultsFor4.push(calculateCountForNumberAndCycles([i], 4));
+    
     }
+    console.log("numbersAndResultsFor4:");
+    console.log(numbersAndResultsFor4);
+
+    //now optimize by replaceing number 1 to 9 by a 0
+
     console.log("answer1:");
-    console.log(stones.length);
+    answer1 = calculateCountForNumberAndCycles(stones, 25);
+    console.log(answer1);//198075
+
   });
+
+function calculateCountForNumberAndCycles(numbers, cycles) {
+  for (let i = 0; i < cycles; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      numbers[j] = step(numbers[j]);
+    }
+    numbers = numbers.flat();
+    //console.log("cycle " + i);
+  }
+  console.log("numbers:");
+  console.log(numbers);
+  return numbers.length;
+}
+
+function step(input) {
+  if (input == "0") {
+    //rule 1
+    input = "1";
+  } else if (input.toString().length % 2 == 0) {
+    //rule 2
+    const partOne = input.toString().slice(0, input.toString().length / 2);
+    let partTwo = input
+      .toString()
+      .slice(input.toString().length / 2, input.toString().length);
+    while (partTwo.split("")[0] == 0) {
+      partTwo = partTwo.split("").slice(1, partTwo.length).join("");
+    }
+    if (partTwo == "") {
+      partTwo = "0";
+    }
+    input = [partOne, partTwo];
+  } else {
+    //rule 3
+    let bigNumber = BigInt(input);
+    bigNumber = bigNumber * 2024n;
+    input = bigNumber.toString();
+  }
+  return input;
+}
