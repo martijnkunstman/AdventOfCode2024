f0baf0abd610bcb61e590b589d28423d9650a837
Martijn Kunstman <martijn@kunstman.nl>
2024-12-13 18:54:23 +0100

    day 13 part 1

--- day13/main.js
+++ day13/main.js
@@ -4,19 +4,67 @@ let data = [];
 /* structure
 
 [
-{stepX: 94, stepY: 34, targetX: 8400, targetY: 5400},
+{AstepX: 94, AstepY: 34, BstepX: 22, BstepY: 67, targetX: 8400, targetY: 5400},
 ...
 ]
 
 */
 
-//fetch("data.txt")
-fetch("data_simple.txt")
+fetch("data.txt")
+//fetch("data_simple.txt")
   .then((response) => response.text())
   .then((textData) => {
-    data = textData.split("\r\n");
+    let temp = textData.split("\r\n");
+    for (let i = 0; i < temp.length; i++) {
+      temp[i] = temp[i].split(" ").map((x) => x.replace(/\D/g, ""));
+    }
+    console.log(temp);
+    for (i = 0; i < temp.length; i += 4) {
+      data.push({
+        AstepX: parseInt(temp[i][2]),
+        AstepY: parseInt(temp[i][3]),
+        BstepX: parseInt(temp[i + 1][2]),
+        BstepY: parseInt(temp[i + 1][3]),
+        targetX: parseInt(temp[i + 2][1]),
+        targetY: parseInt(temp[i + 2][2]),
+      });
+    }
+
     for (let i = 0; i < data.length; i++) {
-      data[i] = data[i].split("");
-    }   
-    console.log(data);
+      //minimal A presses
+      answer1 = answer1 + solve(data[i]);
+    }
+
+    //test data = 480
+    console.log("Answer 1: " + answer1);
   });
+
+function solve(data) {
+  console.log(data);
+  let tokens = 0;
+  let AX = Number(data.AstepX);
+  let AY = Number(data.AstepY);
+  let BX = Number(data.BstepX);
+  let BY = Number(data.BstepY);
+  let targetX = Number(data.targetX);
+  let targetY = Number(data.targetY);
+
+  for (let apress = 0; apress < 100; apress++) {
+    for (let bpress = 0; bpress < 100; bpress++) {
+      let targetXnow = 0;
+      let targetYnow = 0;
+      targetXnow += AX * apress;
+      targetXnow += BX * bpress;
+      if (targetXnow == targetX) {
+        targetYnow += AY * apress;
+        targetYnow += BY * bpress;
+        if (targetYnow == targetY) {
+          tokens = apress * 3 + bpress;
+          return tokens;
+        }
+      }
+    }
+  }
+
+  return tokens;
+}
