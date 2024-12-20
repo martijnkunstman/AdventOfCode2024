77bb050ba70f0b73854ae550b6a2ee3d9703cbdc
Martijn Kunstman <martijn@kunstman.nl>
2024-12-20 19:56:34 +0100

    day 16 part 1 finally done....

--- day16/main.js
+++ day16/main.js
@@ -16,10 +16,10 @@ let graph = {};
 let cytoscapeData;
 let foundPath = [];
 
-//fetch("data.txt")
-fetch("data_test.txt")
-//fetch("data_simple.txt")//7036
-//fetch("data_bigger.txt")//11048
+fetch("data.txt")
+//fetch("data_test.txt")
+  //fetch("data_simple.txt")//7036
+  //fetch("data_bigger.txt")//11048
   .then((response) => response.text())
   .then((textData) => {
     let temp = textData.split("\r\n");
@@ -38,14 +38,10 @@ fetch("data_test.txt")
     //checkSolution(startPosition);
     //
 
-   
-
     let tempMap = [...mapData];
     //console.log(Object.keys(graph));
     let keysTemp = Object.keys(graph);
-    //visualizeGraphOnMap();
-   
-
+    visualizeGraphOnMap();
 
     function visualizeGraphOnMap() {
       for (let i = 0; i < keysTemp.length; i++) {
@@ -55,80 +51,67 @@ fetch("data_test.txt")
         console.log(x + " " + y);
         tempMap[y][x] = "X";
       }
-      
+
       let temp = "";
       for (let y = 0; y < tempMap.length; y++) {
-        temp = temp + tempMap[y].join("")+ "\n";
+        temp = temp + tempMap[y].join("") + "\n";
       }
       console.log("tempMap");
       console.log(temp);
     }
-    
+
     let startNode = "n" + startPosition.x + "-" + startPosition.y;
     let endNode = "n" + endPosition.x + "-" + endPosition.y;
-    
 
     cytoscapeData = convertToCytoscape(graph);
     //console.log("cytoscapeData");
     //console.log(cytoscapeData);
 
+    //cytoscapeData = convertToUndirected(cytoscapeData)
 
-    //cytoscapeData = convertToUndirected(cytoscapeData) 
-    
     //console.log(cytoscapeData);
-    visualizeGraph()
-
+    //visualizeGraph();
 
     // Initialize Cytoscape.js
-    const cy = cytoscape({elements: cytoscapeData.elements});  
-    //var dijkstra = cy.elements().dijkstra({root:"#"+startNode, goal:"#"+endNode, weight: function(edge){return edge.data('weight');}});    
-    
+    const cy = cytoscape({ elements: cytoscapeData.elements });
+    //var dijkstra = cy.elements().dijkstra({root:"#"+startNode, goal:"#"+endNode, weight: function(edge){return edge.data('weight');}});
 
-    var dijkstra = cy.elements().dijkstra("#"+startNode, function(edge){
-      return edge.data('weight');
+    var dijkstra = cy.elements().dijkstra("#" + startNode, function (edge) {
+      return edge.data("weight");
     });
-    
-    var pathToJ = dijkstra.pathTo( cy.$("#"+endNode) );
-    var distToJ = dijkstra.distanceTo( cy.$("#"+endNode) );
+
+    var pathToJ = dijkstra.pathTo(cy.$("#" + endNode));
+    var distToJ = dijkstra.distanceTo(cy.$("#" + endNode));
 
     console.log(pathToJ);
     console.log(distToJ);
     ////
-    
-    
-    
- 
+
     let answer1 = 0;
-   
- 
 
     for (let i = 0; i < pathToJ.length; i++) {
       let temp = pathToJ[i]._private.data.id;
       //first letter starts with an n
-     if (i%2 == 0) {    
+      if (i % 2 == 0) {
         foundPath.push(temp);
         //console.log(pathToJ[i]);
         answer1 += pathToJ[i]._private.data.weight;
         //answer1 += 1000;
       }
     }
-    //foundPath = [...new Set(foundPath)];  
+    //foundPath = [...new Set(foundPath)];
     //console.log(foundPath);
 
     console.log("answer1: " + answer1);
     console.log("foundPath");
     console.log(foundPath);
 
-     
     //mame a path on the map from the foundPath
     //foundPath.splice(4, 1);
     //foundPath.splice(70, 1);
 
-    
-
-
     for (let i = 0; i < foundPath.length; i++) {
-      let temp = foundPath[i].replaceAll("n","").split("-");
+      let temp = foundPath[i].replaceAll("n", "").split("-");
       let x = parseInt(temp[0]);
       let y = parseInt(temp[1]);
       //console.log(x + " " + y);
@@ -139,14 +122,14 @@ fetch("data_test.txt")
 
     //i got a path with X now calcultate the distance
     let distance = 0;
-    for (let i = 0; i < foundPath.length-1; i++) {
-      let temp = foundPath[i].replaceAll("n","").split("-");
+    for (let i = 0; i < foundPath.length - 1; i++) {
+      let temp = foundPath[i].replaceAll("n", "").split("-");
       let x1 = parseInt(temp[0]);
       let y1 = parseInt(temp[1]);
-      let temp2 = foundPath[i+1].replaceAll("n","").split("-");
+      let temp2 = foundPath[i + 1].replaceAll("n", "").split("-");
       let x2 = parseInt(temp2[0]);
       let y2 = parseInt(temp2[1]);
-      distance += Math.abs(x1-x2) + Math.abs(y1-y2);
+      distance += Math.abs(x1 - x2) + Math.abs(y1 - y2);
       distance += 1000;
     }
     console.log("distance: " + distance);
@@ -154,8 +137,6 @@ fetch("data_test.txt")
     //112436 to high
     //101508 to high
     //112436 A star
-
-
 
     //101484 not right
     //100048 not right
@@ -178,84 +159,77 @@ fetch("data_test.txt")
 
     //98476 not good...
 
-
-    
-
-  
-    
-
-
+    //99460
   });
-  
-  function convertToUndirected(graphData) {
-    const edges = graphData.elements.edges;
-    const undirectedEdges = new Set();
-  
-    // Iterate through the edges to ensure bidirectionality
-    edges.forEach(edge => {
-      const source = edge.data.source;
-      const target = edge.data.target;
-      const weight = edge.data.weight;
-  
-      // Create a unique key for the edge
-      const forwardKey = `${source}-${target}`;
-      const reverseKey = `${target}-${source}`;
-  
-      // Add the edge if neither direction exists in the set
-      if (!undirectedEdges.has(forwardKey) && !undirectedEdges.has(reverseKey)) {
-        undirectedEdges.add(forwardKey); // Mark the forward edge
-        undirectedEdges.add(reverseKey); // Mark the reverse edge
-  
-        // Add both directions to the undirected graph
-        edges.push({ data: { source: target, target: source, weight } });
-      }
-    });
-  
-    return graphData;
-  }
-  
 
+function convertToUndirected(graphData) {
+  const edges = graphData.elements.edges;
+  const undirectedEdges = new Set();
 
-  function visualizeGraph() {
-    // Initialize Cytoscape.js
-    const cy = cytoscape({
-      container: document.getElementById('cy'), // Container to render the graph
-      elements: cytoscapeData.elements,
-      style: [ // Define styles for nodes and edges
-        {
-          selector: 'node',
-          style: {
-            'background-color': '#0074D9',
-            'label': 'data(id)',
-            'color': '#fff',
-            'text-valign': 'center',
-            'text-halign': 'center',
-            'font-size': '12px'
-          }
+  // Iterate through the edges to ensure bidirectionality
+  edges.forEach((edge) => {
+    const source = edge.data.source;
+    const target = edge.data.target;
+    const weight = edge.data.weight;
+
+    // Create a unique key for the edge
+    const forwardKey = `${source}-${target}`;
+    const reverseKey = `${target}-${source}`;
+
+    // Add the edge if neither direction exists in the set
+    if (!undirectedEdges.has(forwardKey) && !undirectedEdges.has(reverseKey)) {
+      undirectedEdges.add(forwardKey); // Mark the forward edge
+      undirectedEdges.add(reverseKey); // Mark the reverse edge
+
+      // Add both directions to the undirected graph
+      edges.push({ data: { source: target, target: source, weight } });
+    }
+  });
+
+  return graphData;
+}
+
+function visualizeGraph() {
+  // Initialize Cytoscape.js
+  const cy = cytoscape({
+    container: document.getElementById("cy"), // Container to render the graph
+    elements: cytoscapeData.elements,
+    style: [
+      // Define styles for nodes and edges
+      {
+        selector: "node",
+        style: {
+          "background-color": "#0074D9",
+          label: "data(id)",
+          color: "#fff",
+          "text-valign": "center",
+          "text-halign": "center",
+          "font-size": "12px",
         },
-        {
-          selector: 'edge',
-          style: {
-            'width': 2,
-            'line-color': '#FF4136',
-            'target-arrow-color': '#FF4136',
-            'target-arrow-shape': 'triangle',
-            'curve-style': 'bezier',
-            'label': 'data(weight)',
-            'font-size': '10px',
-            'text-background-opacity': 1,
-            'text-background-color': '#fff',
-            'text-background-padding': '2px',
-            'color': '#111'
-          }
-        }
-      ],
-      layout: {
-        name: 'grid', // Layout algorithm (you can try 'grid', 'circle', etc.)
-        padding: 10
-      }
-    });
-  }
+      },
+      {
+        selector: "edge",
+        style: {
+          width: 2,
+          "line-color": "#FF4136",
+          "target-arrow-color": "#FF4136",
+          "target-arrow-shape": "triangle",
+          "curve-style": "bezier",
+          label: "data(weight)",
+          "font-size": "10px",
+          "text-background-opacity": 1,
+          "text-background-color": "#fff",
+          "text-background-padding": "2px",
+          color: "#111",
+        },
+      },
+    ],
+    layout: {
+      name: "grid", // Layout algorithm (you can try 'grid', 'circle', etc.)
+      padding: 10,
+    },
+  });
+}
 
 function convertToCytoscape(data) {
   const cytoscapeData = { elements: { nodes: [], edges: [] } };
@@ -265,7 +239,15 @@ function convertToCytoscape(data) {
   for (const source in data) {
     // Add source node if not already added
     if (!seenNodes.has(source)) {
-      cytoscapeData.elements.nodes.push({ data: { id: source, position: {x: Number(source.replace("n","").split("-")[0]), y: Number(source.replace("n","").split("-")[1]) }} });
+      cytoscapeData.elements.nodes.push({
+        data: {
+          id: source,
+          position: {
+            x: Number(source.replace("n", "").split("-")[0]),
+            y: Number(source.replace("n", "").split("-")[1]),
+          },
+        },
+      });
       seenNodes.add(source);
     }
 
@@ -278,7 +260,15 @@ function convertToCytoscape(data) {
 
       // Add target node if not already added
       if (!seenNodes.has(target)) {
-        cytoscapeData.elements.nodes.push({ data: { id: target, position: {x: Number(target.replace("n","").split("-")[0]), y: Number(target.replace("n","").split("-")[1]) }} });
+        cytoscapeData.elements.nodes.push({
+          data: {
+            id: target,
+            position: {
+              x: Number(target.replace("n", "").split("-")[0]),
+              y: Number(target.replace("n", "").split("-")[1]),
+            },
+          },
+        });
         seenNodes.add(target);
       }
 
@@ -338,7 +328,6 @@ function consoleLogMap() {
   temp = temp.replaceAll("0", ".");
   //temp = temp.replaceAll("#", "■");
 
-  
   //
   let mazeHTML = document.getElementById("maze");
   for (let X = 0; X < mapData[0].length; X++) {
@@ -356,21 +345,24 @@ function consoleLogMap() {
     }
   }
 
-  document.getElementById("x" + startPosition.x + "y" + startPosition.y).style.backgroundColor = "green"; 
-  document.getElementById("x" + endPosition.x + "y" + endPosition.y).style.backgroundColor = "red";
-
+  document.getElementById(
+    "x" + startPosition.x + "y" + startPosition.y
+  ).style.backgroundColor = "green";
+  document.getElementById(
+    "x" + endPosition.x + "y" + endPosition.y
+  ).style.backgroundColor = "red";
 
   let previousPoint;
   for (let i = 0; i < foundPath.length; i++) {
-    let temp = foundPath[i].replaceAll("n","").split("-");
+    let temp = foundPath[i].replaceAll("n", "").split("-");
     let x = parseInt(temp[0]);
     let y = parseInt(temp[1]);
-    if (i>0) {
-      let temp2 = foundPath[i-1].replaceAll("n","").split("-");
+    if (i > 0) {
+      let temp2 = foundPath[i - 1].replaceAll("n", "").split("-");
       let x2 = parseInt(temp2[0]);
       let y2 = parseInt(temp2[1]);
       if (x2 == x) {
-        for (let j = 1; j < Math.abs(y2-y); j++) {
+        for (let j = 1; j < Math.abs(y2 - y); j++) {
           if (y2 < y) {
             //document.getElementById("x" + x + "y" + (y2+j)).style.backgroundColor = "blue";
           } else {
@@ -379,7 +371,7 @@ function consoleLogMap() {
         }
       }
       if (y2 == y) {
-        for (let j = 1; j < Math.abs(x2-x); j++) {
+        for (let j = 1; j < Math.abs(x2 - x); j++) {
           if (x2 < x) {
             //document.getElementById("x" + (x2+j) + "y" + y).style.backgroundColor = "blue";
           } else {
@@ -394,112 +386,118 @@ function consoleLogMap() {
     document.getElementById("x" + x + "y" + y).classList.add("node");
     console.log(x + " " + y);
   }
+}
 
-
-
+function isNode(x, y) {
+  let isNode = false;
+  //case 1: 3 or 4 empty spaces around it
+  if (x == startPosition.x && y == startPosition.y) {
+    isNode = true;
+  }
+  if (x == endPosition.x && y == endPosition.y) {
+    isNode = true;
+  }
+  if (mapData[y][x] == ".") {
+    let count = 0;
+    if (mapData[y + 1][x] == ".") {
+      count++;
+    }
+    if (mapData[y - 1][x] == ".") {
+      count++;
+    }
+    if (mapData[y][x + 1] == ".") {
+      count++;
+    }
+    if (mapData[y][x - 1] == ".") {
+      count++;
+    }
+    if (count > 2) {
+      isNode = true;
+    }
+    //case 2: 2 empty spaces around it and they are not in the same direction
+    count = 0;
+    if (mapData[y + 1][x] == ".") {
+      count++;
+    }
+    if (mapData[y - 1][x] == ".") {
+      count++;
+    }
+    if (mapData[y][x + 1] == ".") {
+      count++;
+    }
+    if (mapData[y][x - 1] == ".") {
+      count++;
+    }
+    if (count == 2) {
+      if (
+        (mapData[y + 1][x] == "." && mapData[y - 1][x] == ".") ||
+        (mapData[y][x + 1] == "." && mapData[y][x - 1] == ".")
+      ) {
+        isNode = false;
+      } else {
+        isNode = true;
+      }
+    }
+  }
+  return isNode;
 }
 
 function makeGraph() {
   //create a graph from the map based on all nodes... nodes are corners
   for (let y = 0; y < mapData.length; y++) {
     for (let x = 0; x < mapData[y].length; x++) {
-      if (mapData[y][x] == ".") {
-        //check if this is a node...
-        let isNode = false;
-        //case 1: 3 or 4 empty spaces around it
-        let count = 0;
-        if (mapData[y + 1][x] == ".") {
-          count++;
-        }
-        if (mapData[y - 1][x] == ".") {
-          count++;
-        }
-        if (mapData[y][x + 1] == ".") {
-          count++;
-        }
-        if (mapData[y][x - 1] == ".") {
-          count++;
-        }
-        if (count > 2) {
-          isNode = true;
-        }
-        //case 2: 2 empty spaces around it and they are not in the same direction
-        count = 0;
-        if (mapData[y + 1][x] == ".") {
-          count++;
-        }
-        if (mapData[y - 1][x] == ".") {
-          count++;
-        }
-        if (mapData[y][x + 1] == ".") {
-          count++;
-        }
-        if (mapData[y][x - 1] == ".") {
-          count++;
-        }
-        if (count == 2) {
-          if (
-            (mapData[y + 1][x] == "." && mapData[y - 1][x] == ".") ||
-            (mapData[y][x + 1] == "." && mapData[y][x - 1] == ".")
-          ) {
-            isNode = false;
-          } else {
-            isNode = true;
-          }
-        }
-
-        if (isNode) {
-          let key = "n" + x + "-" + y;
-          //find all nodes around this node and the distance to them
-          graph[key] = [];
-          //move up as far as possible
-          let tempY = y;
-          while (mapData[tempY - 1][x] == ".") {
-            tempY--;
-          }
-          if (tempY != y) {
+      if (isNode(x, y)) {
+        let key = "n" + x + "-" + y;
+        //find all nodes around this node and the distance to them
+        graph[key] = [];
+        //move up as far as possible
+        let tempY = y;
+        while (mapData[tempY - 1][x] == ".") {
+          if (isNode(x, tempY - 1)) {
             graph[key].push({
-              n: "n" + x + "-" + tempY,
-              d: Math.abs(y - tempY)+1000,
+              n: "n" + x + "-" + (tempY - 1),
+              d: Math.abs(y - (tempY - 1)) + 1000,
             });
           }
-          //move down as far as possible
-          tempY = y;
-          while (mapData[tempY + 1][x] == ".") {
-            tempY++;
-          }
-          if (tempY != y) {
+          tempY--;
+        }
+        //move down as far as possible
+        tempY = y;
+        while (mapData[tempY + 1][x] == ".") {
+          if (isNode(x, tempY + 1)) {
             graph[key].push({
-              n: "n" + x + "-" + tempY,
-              d: Math.abs(y - tempY)+1000,
+              n: "n" + x + "-" + (tempY + 1),
+              d: Math.abs(y - (tempY + 1)) + 1000,
             });
           }
-          //move left as far as possible
-          let tempX = x;
-          while (mapData[y][tempX - 1] == ".") {
-            tempX--;
-          }
-          if (tempX != x) {
+          tempY++;
+        }
+        //move left as far as possible
+        let tempX = x;
+        while (mapData[y][tempX - 1] == ".") {
+          if (isNode(tempX - 1, y)) {
             graph[key].push({
-              n: "n" + tempX + "-" + y,
-              d: Math.abs(x - tempX)+1000,
+              n: "n" + (tempX - 1) + "-" + y,
+              d: Math.abs(x - (tempX - 1)) + 1000,
             });
           }
-          //move right as far as possible
-          tempX = x;
-          while (mapData[y][tempX + 1] == ".") {
-            tempX++;
-          }
-          if (tempX != x) {
+          tempX--;
+        }
+        //move right as far as possible
+        tempX = x;
+        while (mapData[y][tempX + 1] == ".") {
+          if (isNode(tempX + 1, y)) {
             graph[key].push({
-              n: "n" + tempX + "-" + y,
-              d: Math.abs(x - tempX)+1000,
+              n: "n" + (tempX + 1) + "-" + y,
+              d: Math.abs(x - (tempX + 1)) + 1000,
             });
           }
+          tempX++;
         }
       }
     }
   }
+
   //console.log("Graph-length: " + Object.keys(graph).length);
   //console.log(graph);
   //console.log("done");
